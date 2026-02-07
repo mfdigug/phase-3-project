7d4620b73d3f7583cf27c100abbc1bd0dff8351b
Michelle Di Guglielmo <mfdigug@gmail.com>
2026-02-07 13:16:59 +1100

    created MenuItems model

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,19 @@
-#!/usr/bin/env python3
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, Float, String
 from sqlalchemy.ext.declarative import declarative_base
+# from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('sqlite:///cafe.db')
 Base = declarative_base()
+
+
+class MenuItems(Base):
+    __tablename__ = "menu_items"
+
+    id = Column(Integer(), primary_key=True)
+    item = Column(String(), nullable=False)
+    price = Column(Float(), nullable=False)
+
+    def __repr__(self):
+        return f"Menu item {self.id}: " \
+            + f"{self.item}, " \
+            + f"{self.price}"
